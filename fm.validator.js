e29d4fb52225c2c76a91746d1314acff1be3cefc
Ingi P. Jacobsen <ingi@faroemedia.com>
2014-01-31 17:28:39 +0100

    Updated comments

--- fm.validator.js
+++ fm.validator.js
@@ -1,45 +1,29 @@
-/**
- * Validator v1.3 - copyright 2013 Faroe Media
- * All rights reserved
- * No use of this code is allowed
- * Any use of this code is prohibited and will be prosecuted
- * 
- * input[type=text]
- * 	data-required
- * 	data-required-if=element
- * 	data-min=0-9
- * 	data-max=0-9
- * 	data-type=email,url,number,digits
- * 	data-error-position=before,after,before parentelement,after parentelement
- * 
- * input[type=password]
- * 	data-required
- * 	data-required-if=element
- * 	data-min=0-9
- * 	data-max=0-9
- * 	data-match=element
- * 	data-error-position=before,after,before parentelement,after parentelement
- * 
- * input[type=checkbox]
- * 	data-required
- * 	data-required-if=element
- * 	data-error-position=before,after,before parentelement,after parentelement
- * 
- * select
- * 	data-required
- * 	data-required-if=element
- * 	data-error-position=before,after,before parentelement,after parentelement
- * 
- * textarea
- * 	data-required
- * 	data-required-if=element
- * 	data-min=0-9
- * 	data-max=0-9
- * 	data-type=url
- * 	data-error-position=before,after,before parentelement,after parentelement
- * 
- * 
- * @type Object
+/*
+ Validator jQuery Plugin
+ Validator is a JQuery validation plugin for forms.
+ version 1.3, Jan 31th, 2014
+ by Ingi P. Jacobsen
+
+ The MIT License (MIT)
+
+ Copyright (c) 2014 Faroe Media
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 Validator = {
